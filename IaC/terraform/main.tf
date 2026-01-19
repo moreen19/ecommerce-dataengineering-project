9e456f3ddd338fc05ed104ed87023d887e9448c8
Moreen Andrason <moreenawuor@gmail.com>
2026-01-18 20:48:50 -0500

    feat: add architecture diagrams and update terraform config

--- IaC/terraform/main.tf
+++ IaC/terraform/main.tf
@@ -10,3 +10,16 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+resource "aws_glue_catalog_database" "default_db" {
+  name = "default" 
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "moeandy-terraform-state" # The name of the NEW bucket
+    key            = "state/terraform.tfstate"
+    region         = "us-east-2"
+    encrypt        = true
+  }
+}
